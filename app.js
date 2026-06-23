3bf508096d193c33ad340fce9c85d1d22f23f7f5
nayaba <nabila.ayaba@gmail.com>
2026-06-23 10:00:03 +0300

    Stores the user's choice in a variable

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('what a morning')
+/*-------------- Constants --------------*/
+
+/*-------------- Variables --------------*/
+let userChoice = ''
+
+/*------ Cached Element References ------*/
+const choices = document.querySelector('#choices')
+console.log(choices)
+
+/*----------- Event Listeners -----------*/
+choices.addEventListener('click', function(event){
+    userChoice = event.target.id
+    console.log('userChoice: ', userChoice)
+})
+
+/*-------------- Functions --------------*/

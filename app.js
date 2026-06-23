eed08dbfae85249cb760f3b5f8e5bdc02d7676c6
Fadhel <fadhel.s.hashem@gmail.com>
2026-06-23 10:55:22 +0300

    computer respond

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 /*-------------- Constants --------------*/
+const choiceArr = ['rock', 'paper' , 'scissors']
 
 /*-------------- Variables --------------*/
 let userChoice = ''
+let computerChoice = ''
+let randomIndex = Math.floor(Math.random() *3) ;
 
 /*------ Cached Element References ------*/
 const choices = document.querySelector('#choices')
-console.log(choices)
+
 
 const rock= document.querySelector('#rock')
 const paper= document.querySelector('#paper')
 const scissors= document.querySelector('#scissors')
 const reset = document.querySelector('#reset')
+
+
 /*----------- Event Listeners -----------*/
 //
 choices.addEventListener('click', function(event){
     userChoice = event.target.id
 
-// we wamt to hide buttons that not user choice
+// we wamt to hide buttons that not user choice======
 if (userChoice === 'rock'){
-// add hidden class to paper and scissor
+// add hidden class to paper and scissor=============
 paper.classList.add('hidden')
 scissors.classList.add('hidden')
 } else if (userChoice === 'paper')
@@ -29,7 +34,11 @@ paper.classList.add('hidden')
 rock.classList.add('hidden')
 }
 
+// computer make choice===============================
+computerChoice= choiceArr[0]
+
     console.log('userChoice: ', userChoice)
+    console.log('computerChoice' , computerChoice )
 })
 // how to reset when after hidden
 reset.addEventListener('click', function (){

425f5310c8c95ff2713bd9cc3fd3523a45565443
Fadhel <fadhel.s.hashem@gmail.com>
2026-06-23 11:18:25 +0300

    computer-choice

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 /*-------------- Constants --------------*/
-const choiceArr = ['rock', 'paper' , 'scissors']
+const choiceArr = ['rock', 'paper','scissors']
 
 /*-------------- Variables --------------*/
 let userChoice = ''
 let computerChoice = ''
-let randomIndex = Math.floor(Math.random() *3) ;
+
 
 /*------ Cached Element References ------*/
 const choices = document.querySelector('#choices')
@@ -14,6 +14,7 @@ const rock= document.querySelector('#rock')
 const paper= document.querySelector('#paper')
 const scissors= document.querySelector('#scissors')
 const reset = document.querySelector('#reset')
+const resultDisplay = document.querySelector('#result-display')
 
 
 /*----------- Event Listeners -----------*/
@@ -22,30 +23,47 @@ choices.addEventListener('click', function(event){
     userChoice = event.target.id
 
 // we wamt to hide buttons that not user choice======
-if (userChoice === 'rock'){
+// if (userChoice === 'rock'){
 // add hidden class to paper and scissor=============
-paper.classList.add('hidden')
-scissors.classList.add('hidden')
+// paper.classList.add('hidden')
+// scissors.classList.add('hidden')
+// } else if (userChoice === 'paper')
+//     {rock.classList.add('hidden')
+// scissors.classList.add('hidden')
+// }else if (userChoice === 'scissors'){
+// paper.classList.add('hidden')
+// rock.classList.add('hidden')
+
+// make the button disable instead of hiden===========
+if (userChoice === 'rock'){
+paper.setAttribute('disabled', 'disabled')
+scissors.setAttribute('disabled', 'disabled')
 } else if (userChoice === 'paper')
-    {rock.classList.add('hidden')
-scissors.classList.add('hidden')
+    {rock.setAttribute('disabled', 'disabled')
+scissors.setAttribute('disabled', 'disabled')
 }else if (userChoice === 'scissors'){
-paper.classList.add('hidden')
-rock.classList.add('hidden')
+paper.setAttribute('disabled', 'disabled')
+rock.setAttribute('disabled', 'disabled')
 }
 
 // computer make choice===============================
-computerChoice= choiceArr[0]
+let randomIndex = Math.floor(Math.random() *3) ;
+computerChoice= choiceArr[randomIndex]
+
+resultDisplay.textContent=`computer Chose ${computerChoice}`
 
     console.log('userChoice: ', userChoice)
     console.log('computerChoice' , computerChoice )
 })
 // how to reset when after hidden
 reset.addEventListener('click', function (){
-    paper.classList.remove('hidden')
-    scissors.classList.remove('hidden')
-    rock.classList.remove('hidden')
-    
+    // paper.classList.remove('hidden')
+    // scissors.classList.remove('hidden')
+    // rock.classList.remove('hidden')
+    // Also to make reset butto work with disable=====
+    paper.removeAttribute('disabled')
+    scissors.removeAttribute('disabled')
+    rock.removeAttribute('disabled')
 })
 
 /*-------------- Functions --------------*/

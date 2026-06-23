f160416f055bb0d6b9668de8a080fe57fe118264
Fadhel <fadhel.s.hashem@gmail.com>
2026-06-23 10:39:21 +0300

    my edit 1

--- app.js
+++ app.js
@@ -7,10 +7,36 @@ let userChoice = ''
 const choices = document.querySelector('#choices')
 console.log(choices)
 
+const rock= document.querySelector('#rock')
+const paper= document.querySelector('#paper')
+const scissors= document.querySelector('#scissors')
+const reset = document.querySelector('#reset')
 /*----------- Event Listeners -----------*/
+//
 choices.addEventListener('click', function(event){
     userChoice = event.target.id
+
+// we wamt to hide buttons that not user choice
+if (userChoice === 'rock'){
+// add hidden class to paper and scissor
+paper.classList.add('hidden')
+scissors.classList.add('hidden')
+} else if (userChoice === 'paper')
+    {rock.classList.add('hidden')
+scissors.classList.add('hidden')
+}else if (userChoice === 'scissors'){
+paper.classList.add('hidden')
+rock.classList.add('hidden')
+}
+
     console.log('userChoice: ', userChoice)
+})
+// how to reset when after hidden
+reset.addEventListener('click', function (){
+    paper.classList.remove('hidden')
+    scissors.classList.remove('hidden')
+    rock.classList.remove('hidden')
+    
 })
 
 /*-------------- Functions --------------*/
